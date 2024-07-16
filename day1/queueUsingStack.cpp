846bcaf4f7fa38f6465f1c791d417fe6d8cb104b
soham2403 <116299716+soham2403@users.noreply.github.com>
2024-07-16 12:28:36 +0530

    Update queueUsingStack.cpp

--- day1/queueUsingStack.cpp
+++ day1/queueUsingStack.cpp
@@ -1,16 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-class QueueBrute{
-	stack<int> inp,out;
-	public:
-		void push(){
-		
-		}
+class MyQueue {
 
-		int pop(){
-		
-		}
+public:
+    stack<int> s1, s2;
+    MyQueue() {}
+
+    void push(int x) { s1.push(x); }
+
+    int pop() {
+        if (s2.empty()) {
+            while (s1.size()) {
+                int ele = s1.top();
+                s1.pop();
+                s2.push(ele);
+            }
+        }
+        int ele = s2.top();
+        s2.pop();
+        return ele;
+    }
+
+    int peek() {
+        if (s2.empty()) {
+            while (s1.size()) {
+                int ele = s1.top();
+                s1.pop();
+                s2.push(ele);
+            }
+        }
+        return s2.top();
+    }
+
+    bool empty() { return s1.empty() && s2.empty(); }
 };
+
 int main(){
+	MyQueue* obj = new MyQueue();
+	obj->push(12);
+	int param_2 = obj->pop();
+	int param_3 = obj->peek();
+	bool param_4 = obj->empty();
 	return 0;
 }
